ea985b6af6f18d6994c86f39facff286cb745b0f
xxsamiraf <samiradg105@gmail.com>
2022-05-06 08:24:41 -0400

    update

--- TIL with python/TIL Website/js/show_rooms.js
+++ TIL with python/TIL Website/js/show_rooms.js
@@ -482,8 +482,8 @@ Rooms.prototype.showRoomsBy = function(type) { // type is either 'date' or 'buil
   if (type == 'date') {
     // sort by year first and then by season
     categories.sort(function(a, b) {
-      var yearA = a == 'Pilot' ? 3000 : parseInt(a.slice(-4));
-      var yearB = b == 'Pilot' ? 3000 : parseInt(b.slice(-4));
+      var yearA = a == 'Pilot' ? 0 : parseInt(a.slice(-4));
+      var yearB = b == 'Pilot' ? 0 : parseInt(b.slice(-4));
       var season1ForA = parseInt(a.charAt(0));
       var season2ForA = parseInt(a.charAt(2));
       var season1ForB = parseInt(b.charAt(0));
